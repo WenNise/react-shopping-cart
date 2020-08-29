ae908f043712720d2fa62bdb176843df2d5dccd1
WenNise <wennise@1utar.my>
2020-08-29 17:43:23 +0800

    Added travis.yml file

--- wdio.conf.js
+++ wdio.conf.js
@@ -35,16 +35,18 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
-  capabilities: [
-    {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      maxInstances: 5,
-      //
-      browserName: 'chrome'
+  capabilities: [{
+    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    // grid with only 5 firefox instances available you can make sure that not more than
+    // 5 instances get started at a time.
+    maxInstances: 5,
+    //
+    browserName: 'chrome',
+
+    chromeOptions: {
+        args: ['headless', 'disable-gpu']
     }
-  ],
+}],
   //
   // ===================
   // Test Configurations
